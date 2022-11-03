52169f5afa69278ca4f4c88671b2678669edcee7
nicolethoen <nthoen@redhat.com>
2022-11-03 09:04:13 -0400

    chore: update token again

--- .github/release.sh
+++ .github/release.sh
@@ -5,7 +5,7 @@ REPO="github.com/${GH_REPO}"
 echo "Preparing release environment..."
 git config user.email "patternfly-build@redhat.com"
 git config user.name ${GIT_USERNAME}
-echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" > ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_SECRET}" > ~/.npmrc
 
 echo "Doing a release..."
 # Lerna is complicated. Commands: https://github.com/lerna/lerna/tree/master/commands
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # pf-react-release-playground
 Used for testing the pf-react build and release workflows
+
+npm install && npm run build
